07a04e606d47dbbe2c4faaf1af17a0ccb14ffcf3
Devika Boddu <bharathb.chander@gmail.com>
2026-09-15 03:00:58 +0530

    build: add Remotion composition entrypoint

---
+++ remotion/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Composition} from 'remotion';
+import {Documentary} from './Documentary';
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <Composition
+      id="Documentary"
+      component={Documentary}
+      durationInFrames={900}
+      fps={30}
+      width={1920}
+      height={1080}
+      defaultProps={{
+        title: 'THE CHIP THAT CHANGED EVERYTHING',
+        subtitle: 'A FLVYT documentary prototype',
+      }}
+    />
+  );
+};
